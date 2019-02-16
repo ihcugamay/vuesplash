d8b74e6c88776d696d1b9429542ab9082f11cd08
ihcugamay <ymgc1994@icloud.com>
2019-02-16 22:25:45 +0900

    chapter-9 finished

---
+++ app/Http/Controllers/PhotoController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePhoto;
+use App\Photo;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+
+class PhotoController extends Controller
+{
+    public function __construct()
+    {
+        // 認証が必要
+        $this->middleware('auth');
+    }
+
+    public function create(StorePhoto $request)
+    {
+        // 投稿写真の拡張子を取得する
+        $extension = $request->photo->extension();
+
+        $photo = new Photo();
+
+        // インスタンス生成時に割り振られたランダムなid値と本来の拡張子を組み合わせてファイル名とする
+        $photo->filename = $photo->id . '.' . $extension;
+
+        // s3にファイルを保存する
+        // 第3引数の'public'はファイルを公開状態で保存するため
+        Storage::cloud()->putFileAs('', $request->photo, $photo->filename, 'public');
+
+        //データベースエラー時にファイル削除を行うためトランザクションを利用する
+        DB::beginTransaction();
+
+        try {
+            Auth::user()->photos()->save($photo);
+            DB::commit();
+        } catch (\Exception $exception) {
+            DB::rollback();
+            // DBとの不整合を避けるためアップロードしたファイルを削除
+            Storage::cloud()->delete($photo->filename);
+            throw $exception;
+        }
+
+        // リソースの新規作成なのでレスポンスコードは201(CREATED)を返却する
+        return response($photo, 201);
+    }
+}
