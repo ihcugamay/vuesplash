44e93d5bcde706063b70b6694dd3745aff5a8299
ihcugamay <ymgc1994@icloud.com>
2019-02-17 23:56:57 +0900

    chapter-10 finished

--- resources/js/router.js
+++ resources/js/router.js
@@ -4,6 +4,7 @@ import VueRouter from "vue-router";
 import PhotoList from "./pages/PhotoList.vue";
 import Login from "./pages/Login.vue";
 import SystemError from "./pages/errors/System.vue";
+import PhotoDetail from "./pages/PhotoDetail.vue";
 
 import store from "./store";
 
@@ -13,6 +14,11 @@ const routes = [
   {
     path: "/",
     component: PhotoList
+  },
+  {
+    path: "/photos/:id",
+    component: PhotoDetail,
+    props: true
   },
   {
     path: "/login",
